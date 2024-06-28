8c6fcde9845154820b04a9657d9a5d00685c0e92
DannyN29 <danny1999navarrete@gmail.com>
2024-06-28 00:15:14 -0500

    Se añadió código para que el usuario digite la placa, fecha y hora en la que despues esta un if para determinar si cumplen con las restricciones si es asi le aparece el mensaje Pico y Placa

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ var rl = readline.createInterface({
 });
 // Función para obtener el día de la semana de una fecha dada
 function obtenerDiaSemana(fecha) {
-    var fechaMoment = moment(fecha, 'DD/MM/YYYY');
     // Convertimos la fecha ingresada al formato 'dddd' para obtener el día de la semana
+    var fechaMoment = moment(fecha, 'DD/MM/YYYY');
     var diaSemana = fechaMoment.format('dddd');
     return diaSemana;
 }
+// Función para verificar si una hora esta dentro de un horario
 function verificarHorario(hora) {
     // Separamos la hora tanto la  inicial como la final en partes (horas y minutos)
     var partesDeHoraInicial = hora.split(':');
@@ -25,25 +26,43 @@ function verificarHorario(hora) {
     var horasFinal = parseInt(partesDeHoraFinal[0]);
     var minutosFinal = parseInt(partesDeHoraFinal[1]);
     // Verificamos si la hora está dentro del rango deseado
-    if ((horasInicial >= 6 && minutosInicial >= 0 && horasFinal < 9
+    if ((horasInicial >= 6 && minutosInicial >= 0 && horasFinal <= 9
         && minutosFinal >= 0 && minutosFinal <= 30) ||
         (horasInicial >= 16 && minutosInicial >= 0 &&
-            horasFinal <= 21 && minutosFinal >= 0 && minutosFinal <= 59)) {
+            horasFinal <= 21 && minutosFinal == 0)) {
         return true;
     }
     else {
         return false;
     }
 }
-// Pedimos al usuario que ingrese una fecha
-rl.question('Ingrese una fecha (DD/MM/AAAA): ', function (fecha) {
-    // Pedimos al usuario que ingrese una hora
-    rl.question('Ingrese una hora (HH:MM): ', function (hora) {
-        // Obtenemos el día de la semana de la fecha ingresada
-        var diaSemana = obtenerDiaSemana(fecha);
-        console.log("La fecha ".concat(fecha, " es un ").concat(diaSemana));
-        console.log("La hora es: ".concat(hora));
-        // Cerramos la interfaz de lectura
-        rl.close();
+// Pedimos al usuario que ingrese su número de placa
+rl.question('Ingrese su número de placa: ', function (placa) {
+    // Pedimos al usuario que ingrese una fecha
+    rl.question('Ingrese una fecha (DD/MM/AAAA): ', function (fecha) {
+        // Pedimos al usuario que ingrese una hora
+        rl.question('Ingrese una hora (HH:MM): ', function (hora) {
+            // Obtenemos el último digito de la placa ingresada
+            var ultimoCaracter = placa.slice(-1);
+            // Obtenemos el día de la semana de la fecha ingresada
+            var diaSemana = obtenerDiaSemana(fecha);
+            // Verificamos que la hora ingresada este en el horario 
+            var horaIngresada = verificarHorario(hora);
+            console.log("El \u00FAltimo car\u00E1cter de la placa es: ".concat(ultimoCaracter));
+            console.log("La fecha ".concat(fecha, " es un ").concat(diaSemana));
+            console.log("La hora es un ".concat(hora));
+            // Verificamos si el último carácter de la placa, el día de la semana y el horario cumplen las restricciones
+            if (((ultimoCaracter === '1' || ultimoCaracter === '2') && diaSemana === 'Monday') && horaIngresada == true ||
+                ((ultimoCaracter === '3' || ultimoCaracter === '4') && diaSemana === 'Tuesday') && horaIngresada == true ||
+                ((ultimoCaracter === '5' || ultimoCaracter === '6') && diaSemana === 'Wednesday') && horaIngresada == true ||
+                ((ultimoCaracter === '7' || ultimoCaracter === '8') && diaSemana === 'Thursday') && horaIngresada == true ||
+                ((ultimoCaracter === '9' || ultimoCaracter === '0') && diaSemana === 'Friday') && horaIngresada == true) {
+                console.log('Pico y Placa: No puedes circular');
+            }
+            else {
+                console.log("No es Pico y Placa Si puedes circular");
+            }
+            rl.close();
+        });
     });
 });
--- index.ts
+++ index.ts
@@ -13,9 +13,9 @@ const rl = readline.createInterface({
 });
 
 // Función para obtener el día de la semana de una fecha dada
-function obtenerDiaSemana(fecha: Date) {
-    const fechaMoment = moment(fecha, 'DD/MM/YYYY');
+function obtenerDiaSemana(fecha: string) {
     // Convertimos la fecha ingresada al formato 'dddd' para obtener el día de la semana
+    const fechaMoment = moment(fecha, 'DD/MM/YYYY');
     const diaSemana = fechaMoment.format('dddd');
     return diaSemana;
 }
@@ -30,25 +30,43 @@ function verificarHorario(hora: string) {
     const horasFinal = parseInt(partesDeHoraFinal[0]);
     const minutosFinal = parseInt(partesDeHoraFinal[1]);
     // Verificamos si la hora está dentro del rango deseado
-    if ((horasInicial >= 6 && minutosInicial >= 0 && horasFinal < 9
+    if ((horasInicial >= 6 && minutosInicial >= 0 && horasFinal <= 9
         && minutosFinal >= 0 && minutosFinal <= 30) ||
         (horasInicial >= 16 && minutosInicial >= 0 &&
-            horasFinal <= 21 && minutosFinal >= 0 && minutosFinal <= 59)) {
+            horasFinal <= 21 && minutosFinal == 0)) {
         return true;
     } else {
         return false;
     }
 }
 
+// Pedimos al usuario que ingrese su número de placa
+rl.question('Ingrese su número de placa: ', (placa) => {
     // Pedimos al usuario que ingrese una fecha
     rl.question('Ingrese una fecha (DD/MM/AAAA): ', (fecha) => {
-      // Pedimos al usuario que ingrese una hora
-      rl.question('Ingrese una hora (HH:MM): ', (hora) => {
-        // Obtenemos el día de la semana de la fecha ingresada
-        const diaSemana = obtenerDiaSemana(fecha);
-        console.log(`La fecha ${fecha} es un ${diaSemana}`);
-        console.log(`La hora es: ${hora}`);
-        // Cerramos la interfaz de lectura
-        rl.close();
-      });
+        // Pedimos al usuario que ingrese una hora
+        rl.question('Ingrese una hora (HH:MM): ', (hora) => {
+            // Obtenemos el último digito de la placa ingresada
+            const ultimoCaracter = placa.slice(-1);
+            // Obtenemos el día de la semana de la fecha ingresada
+            const diaSemana = obtenerDiaSemana(fecha);
+            // Verificamos que la hora ingresada este en el horario 
+            const horaIngresada = verificarHorario(hora)
+            console.log(`El último carácter de la placa es: ${ultimoCaracter}`);
+            console.log(`La fecha ${fecha} es un ${diaSemana}`);
+            console.log(`La hora es un ${hora}`);
+            // Verificamos si el último carácter de la placa, el día de la semana y el horario cumplen las restricciones
+            if (((ultimoCaracter === '1' || ultimoCaracter === '2') && diaSemana === 'Monday') && horaIngresada == true ||
+                ((ultimoCaracter === '3' || ultimoCaracter === '4') && diaSemana === 'Tuesday') && horaIngresada == true ||
+                ((ultimoCaracter === '5' || ultimoCaracter === '6') && diaSemana === 'Wednesday') && horaIngresada == true ||
+                ((ultimoCaracter === '7' || ultimoCaracter === '8') && diaSemana === 'Thursday') && horaIngresada == true ||
+                ((ultimoCaracter === '9' || ultimoCaracter === '0') && diaSemana === 'Friday') && horaIngresada == true) {
+                console.log('Pico y Placa: No puedes circular');
+            } else {
+                console.log(`No es Pico y Placa Si puedes circular`);
+            }
+            rl.close();
+        });
     });
+});
+
